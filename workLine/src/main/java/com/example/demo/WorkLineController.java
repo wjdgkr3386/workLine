1d4c55d480a6443ee949c3a1f289fd15c23680f7
Jeonghak Kim <wjdgkr3386@naver.com>
2025-06-13 14:57:08 +0900

    workLine 페이지 생성

--- workLine/src/main/java/com/example/demo/WorkLineController.java
+++ workLine/src/main/java/com/example/demo/WorkLineController.java
@@ -1,8 +1,17 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WorkLineController {
 
+	@RequestMapping( value="/workLine.do")
+	public ModelAndView workLine(
+	){
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName( "workLine.jsp" );
+		return mav;
+    }
 }
